eb3fab2d46a14f8099b73418b37bceed20858c20
Sverrir Sigmundarson <sverrirs@gmail.com>
2018-08-29 20:17:12 +0200

    Re implementing offset feature previously done by @benjaminleonard in #34, bumping version

--- lib/jekyll-paginate-v2/version.rb
+++ lib/jekyll-paginate-v2/version.rb
@@ -1,8 +1,10 @@
 module Jekyll
   module PaginateV2
     VERSION = "3.0.0"
+
     # When modifying remember to issue a new tag command in git before committing, then push the new tag
     #   git tag -a v3.0.0 -m "Gem v3.0.0"
+    #34, bumping version
     #   git push origin --tags
     # Yanking a published Gem
     #   gem yank jekyll-paginate-v2 -v VERSION
